4da2f964e1f722b58de41b0d87907b020f10e6d9
Bsian <chharry321@gmail.com>
2024-11-03 23:07:17 +0000

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import { name, version } from "../package.json";
+import { rules as _rules } from "./rules";
 
 export const meta = { name, version };
-export { rules } from "./rules";
+export const rules = _rules;
+
+const exportDefault = {
+  meta,
+  rules,
+};
+
+export default exportDefault;
